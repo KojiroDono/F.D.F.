ecb52eb790a8f0bc16bb76cc964292f5b7ba40ab
aurelien guyon <auguyon@student.42.fr>
2019-01-19 15:07:54 +0100

    Parse: OK

--- fdf.h
+++ fdf.h
@@ -3,15 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nvienot <nvienot@student.42.fr>            +#+  +:+       +#+        */
+/*   By: auguyon <auguyon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/16 11:41:57 by nvienot           #+#    #+#             */
-/*   Updated: 2019/01/17 18:40:55 by nvienot          ###   ########.fr       */
+/*   Created: 2019/01/15 12:11:08 by auguyon           #+#    #+#             */
+/*   Updated: 2019/01/17 11:52:47 by auguyon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include "libft/libft.h"
+# include "mlx/mlx.h"
+# include <fcntl.h>
+# include <stdlib.h>
+# include <math.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 9999
+# define BUF_SIZE 1000
+
+# define RGB(r, g, b)(256 * 256 * (int)(r) + 256 * (int)(g) + (int)(b))
+
+# define BLUE 255
+# define GREEN 65280
+# define RED 16711680
+
+typedef struct		s_gnl
+{
+	char			*tab;
+	int				fd;
+	struct s_gnl	*next;
+}					t_gnl;
 
 typedef struct		s_win
 {
@@ -22,22 +46,14 @@ typedef struct		s_win
 	struct s_win	*next;
 }					t_win;
 
-typedef struct		s_pts
+typedef struct		s_map
 {
-	int x;
-	int y;
-	struct s_pts	*next;
-}					t_pts;
+	char	**map;
+	int		**tab;
+	int		x_max;
+	int		y_max;
+}					t_map;
 
-# define BUF_SIZE 1000
-# define RGB(r, g, b)(256 * 256 * (int)(r) + 256 * (int)(g) + (int)(b))
-# include "./libft/libft.h"
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-#include "mlx.h"
-#include <time.h>
-
-int ft_draw_line(t_win *win, int x1, int x2, int y1, int y2);
+int		get_next_line(const int fd, char **line);
 
 #endif
